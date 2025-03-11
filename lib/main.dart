e11aafda78319ec86ef92461e14592c2023104a6
DANIEL_URIBE_MARTINEZ <a.22308051281323@cbtis128.edu.mx>
2025-03-11 15:36:24 +0000

    imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyListView());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyListView extends StatelessWidget {
+  const MyListView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Daniel Uribe Martinez   Mat:1323'),
+          backgroundColor: const Color(0xff539fff),
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
+          ),
+          leading: const Icon(Icons.menu), // Icono agregado
+        ),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(
+              title: Text('Note Title 1'),
+              subtitle: Text(
+                'Note Content 1',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Note Title 2'),
+              subtitle: Text(
+                'Note Content 2',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Note Title 3'),
+              subtitle: Text(
+                'Note Content 3',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
